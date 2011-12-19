a01ead7a4634ed1ef77a24efc916184054b5ba93
Jamie Kitson <jamie@kitten-x.com>
2011-12-19 14:08:52 +0000

    ditto

--- twitter.php
+++ twitter.php
@@ -2,29 +2,16 @@
 
         include_once 'twitterLink.php';
 
-        // $url = 'http://twitter.com/statuses/user_timeline/jamiekitson.rss?count=10';
-        // foreach($xml->channel->item as $i)
-        // print_r($xml);
-
         $url = 'http://api.twitter.com/1/statuses/user_timeline.xml?screen_name=jamiekitson&count=10&include_rts=1&include_entities=1';
 
         $xml = simplexml_load_string(@file_get_contents($url));
         foreach($xml->status as $i)
         {
                 echo '<div class="twitterpost">';
-                // echo substr(linkify_twitter_status($i->text), 12);
                 $urls = $i->entities->urls;
                 $status = str_replace($urls->url->url, $urls->url->expanded_url, $i->text);
-/*
-		$status = $i->text;
-                foreach($i->entities->urls->url as $u)
-                {
-			$status = str_replace($u->url, $u->expanded_url, $status);
-                }
-*/
                 echo linkify_twitter_status($status);
                 echo '<div class="twitterdate">';
-                // <a class="twitterdate" href="'.$status.'">'.date('D M d H:i', strtotime($i->created_at))."</a>";
                 echo statusLink($i->id, 'jamiekitson', date('D M d H:i', strtotime($i->created_at)));
                 if ($i->in_reply_to_status_id != '')
                         echo statusLink($i->in_reply_to_status_id, $i->in_reply_to_screen_name, 'In reply to '.$i->in_reply_to_screen_name);
