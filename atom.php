6cf11fdc8ccbf585aad5a61fccd324b31ce09583
Jamie Kitson <jamie@kitten-x.com>
2011-12-21 16:13:05 +0000

    Forgot to add geekout and atom

---
+++ atom.php
@@ -0,0 +1,27 @@
+<?php
+
+function atom($url)
+{
+	$xml = simplexml_load_string(@file_get_contents($url));
+
+        foreach($xml->entry as $e)
+        {
+                echo '<div class="postdate">'.date('l, F j, Y', strtotime($e->published))."</div>";
+
+                echo '<h3>';
+                foreach($e->link as $l)
+                {
+			$rel = $l->attributes()->rel[0];
+                        if ($rel == '' || $rel == 'alternate')
+                        {
+                                $link = '<a href="'.$l->attributes()->href[0].'">';
+                                break;
+                        }
+                }
+                echo $link.$e->title."</a></h3>\n";
+                echo '<div class="post">'.substr(strip_tags($e->content), 0, 300);
+                echo $link."...</a></div>\n";
+        }
+}
+
+?>
---
+++ geekout.php
@@ -0,0 +1,9 @@
+<?php
+
+	include_once('atom.php');
+
+        $url = 'http://geekout.org.uk/index.atom';
+
+	atom($url);
+
+?>
