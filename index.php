0a57193d79fe30e0ac448ca3897917f3ed66bf8b
Jamie Kitson <jamie@kitten-x.com>
2012-05-31 11:03:42 +0000

    Capitalise CV

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ $(document).ready(function()
 </a>
 <h1>The Jamie Kitson Portal</h1>
 <a href="&#109;a&#105;&#X6c;&#X0074;&#X6f;&#0058;&#X6a;&#0097;&#0109;&#0105;&#0101;&#00064;k&#X00069;&#116;&#X0074;&#00101;&#0110;&#00045;&#120;&#X2e;&#99;&#111;m">&#X6a;&#0097;&#0109;&#0105;&#0101;&#00064;k&#X00069;&#116;&#X0074;&#00101;&#0110;&#00045;&#120;&#X2e;&#99;&#111;m</a>
-<a href="http://jamiekitson.com">cv</a>
+<a href="http://jamiekitson.com">CV</a>
 <br>
 Mongol Rally: <a href="http://geekout.org.uk">Team Geekout</a> 
 </div>
@@ -88,7 +88,6 @@ Mongol Rally: <a href="http://geekout.org.uk">Team Geekout</a>
 <h2><a href="http://www.flickr.com/photos/jamiekitson">flickr</a></h2>
 <div id="flickrcontainer" class="container">
 <h3><a href="http://www.flickr.com/photos/jamiekitson">my photos</a></h3>
-
 <?php
 
 include_once 'cache.php';
