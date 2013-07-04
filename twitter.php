f0164dbe210a2960248ab9b8b279ea033a828220
Jamie Kitson <jamie@kitten-x.com>
2013-07-04 09:22:19 +0000

    twitter minor refactoring

--- twitter.php
+++ twitter.php
@@ -1,6 +1,6 @@
 <?php
 
-        include_once 'twitterLink.php';
+	include_once 'twitterLink.php';
 	include 'twittertoken.php';
 
 	$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=jamiekitson&count=10&include_rts=1&include_entities=1';
@@ -11,28 +11,27 @@
 
 	$response = curl_exec( $ch );
 
-	$xml = json_decode($response);
+	$tweets = json_decode($response);
 
-        foreach($xml as $i)
+        foreach($tweets as $tweet)
         {
-		$date = strtotime($i->created_at);
-		$id = $i->id;
+		$date = strtotime($tweet->created_at);
 		$status = "";
-		if (property_exists($i, "retweeted_status"))
+		if (property_exists($tweet, "retweeted_status"))
 		{
-			$i = $i->retweeted_status;
-			$status = "RT @".$i->user->screen_name.": ";
+			$tweet = $tweet->retweeted_status;
+			$status = "RT @".$tweet->user->screen_name.": ";
 		}
                 echo '<div class="twitterpost">';
-		$status .= $i->text;
-		expandURLs($i->entities->urls, $status);
-		if (property_exists($i->entities, "media"))
-			expandURLs($i->entities->media, $status);
+		$status .= $tweet->text;
+		expandURLs($tweet->entities->urls, $status);
+		if (property_exists($tweet->entities, "media"))
+			expandURLs($tweet->entities->media, $status);
                 echo linkify_twitter_status($status);
                 echo '<div class="twitterdate">';
-                echo statusLink($id, 'jamiekitson', date('D M d H:i', $date));
-                if ($i->in_reply_to_status_id != '')
-                        echo statusLink($i->in_reply_to_status_id, $i->in_reply_to_screen_name, 'In reply to '.$i->in_reply_to_screen_name);
+                echo statusLink($tweet->id, 'jamiekitson', date('D M d H:i', $date));
+                if ($tweet->in_reply_to_status_id != '')
+                        echo statusLink($tweet->in_reply_to_status_id, $tweet->in_reply_to_screen_name, 'In reply to '.$tweet->in_reply_to_screen_name);
                 echo "</div></div>\n";
         }
 
