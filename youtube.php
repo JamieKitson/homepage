6e445d110b59df3d1bfe8d1138f4f2ed32046564
Jamie Kitson <jamie@kitten-x.com>
2015-05-08 15:04:20 +0000

    update youtube api to v3

--- youtube.php
+++ youtube.php
@@ -3,65 +3,41 @@
 // error_reporting(E_ALL);
 // ini_set('display_errors', '1');
 
+include 'youtubetoken.php';
 
-	$url = "https://gdata.youtube.com/feeds/api/users/jamiekitson/uploads?max-results=5";
+    $url = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=UUFZ1-GCGxmvGvORjxsFEG5w&key=$apikey";
 
 	$a = file_get_contents($url);
+    $vals = json_decode($a);
 
-// print_r($a);
-
-$p = xml_parser_create();
-xml_parse_into_struct($p, $a, $vals, $index);
-xml_parser_free($p);
-
-foreach($vals as $item)
+foreach($vals->items as $item)
 {
-	switch($item['tag']) 
-	{
-		case "PUBLISHED" :
-			$published = $item["value"];
-			$thumb = false; 
-			break;
-		case "TITLE": 
-			$title = $item["value"]; 
-			break;
-		case "CONTENT": 
-			if (array_key_exists("value", $item)) {
-				$desc = $item["value"]; 
-			} else {
-				$desc = "";
-			}
-			break;
-		case "LINK": 
-			if ($item["attributes"]["REL"] == "alternate") 
-			{
-				$link = $item["attributes"]["HREF"];
-			}
-			break;
-		case "MEDIA:THUMBNAIL": 
-			if (!$thumb && ($item["attributes"]["HEIGHT"] == 360))
-			{ 
-				$thumb = '<img src="'.$item["attributes"]["URL"].'" class="ytthumb rel" alt="'.$title.'">';
-			} 
-			break;
-		case "YT:STATISTICS" :
-			$views = $item["attributes"]["VIEWCOUNT"];
-			$favs = $item["attributes"]["FAVORITECOUNT"];
-			print '<a href="'.htmlentities($link).'" class="youtube">'.$thumb;
-			print '<div class="yttext rel"><span class="yttitle">'.$title.'</span><br>';
-			print '<span class="ytdesc">jamiekitson '."$views views<br>$desc</span>";
-			print "</div></a>\n";
-			break;
-	}
+    $ids[] = $item->contentDetails->videoId;
 }
 
-/*
+$sids = implode(",", $ids);
 
-echo "Index array\n";
-print_r($index);
-echo "\nVals array\n";
-print_r($vals);
+$url = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id=".urlencode($sids)."&key=$apikey";
 
-*/
+$a = file_get_contents($url);
+$vals = json_decode($a);
+
+foreach($vals->items as $item)
+{
+
+    $snippet = $item->snippet;
+    $published = $snippet->publishedAt;
+    $title = $snippet->title;
+    $link = "https://www.youtube.com/watch?v=".$item->id;
+    $thumb = '<img src="'.$snippet->thumbnails->medium->url.'" class="ytthumb rel" alt="'.$title.'">';
+    $desc = $snippet->description;
+    $views = $item->statistics->viewCount;
+
+    print '<a href="'.htmlentities($link).'" class="youtube">'.$thumb;
+    print '<div class="yttext rel"><span class="yttitle">'.$title.'</span><br>';
+    print '<span class="ytdesc">jamiekitson '."$views views<br>$desc</span>";
+    print "</div></a>\n";
+
+}
 
 ?>
