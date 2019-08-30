55bf3c6701f7b257859b58b0b04382b273972ffc
Jamie Kitson <jamie@kitten-x.com>
2019-08-30 21:26:32 +0100

    create token files

--- run.sh
+++ run.sh
@@ -2,6 +2,19 @@
 
 #cd ~/homepage
 
+function secret_file {
+cat > $1 << EOF
+<?php
+$2
+?>
+EOF
+}
+
+secret_file "youtubetoken.php" "\$apikey = \"$YOUTUBE_TOKEN\";"
+secret_file "flickr/token.php" "$FLICKR_TOKEN"
+secret_file "flickr/secret.php" "$FLICKR_SECRET"
+secret_file "twittertoken.php" "\$twittertoken = \"$TWITTER_TOKEN\";"
+
 for value in flickrMine flickrFavs flickrOwnFavs flickrMe instagramOwn
 do
     php updateCache.php flickr/$value.php cache/$value.html
@@ -18,7 +31,7 @@ cd flickr
 
 php index.php > index.html
 
-cd ..
+#cd ..
 
 #git commit index.html flickr/index.html cache/* -m "update cache"
 
