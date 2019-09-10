61f7a5b1f101f24258e2593117cb705e7fe13c6c
Jamie Kitson <jamie@kitten-x.com>
2019-09-10 11:47:32 +0100

    add date stamp to cache

--- updateCache.php
+++ updateCache.php
@@ -12,7 +12,7 @@ $s = ob_get_clean();
 
 if (substr_compare($base, 'index', -5) != 0)
 {
-    $s = "\n<!-- cache -->\n$s";
+    $s = "\n<!-- cache ".date("c")." -->\n$s";
     $s = $s.sprintf("\n<!-- %01.2f -->\n", microtime(true) - $t);
 }
 file_put_contents($base.'.html', $s);
