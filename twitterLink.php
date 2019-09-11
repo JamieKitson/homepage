121a07426b55415bd0563028429297510349fa29
Jamie Kitson <jamie@kitten-x.com>
2019-09-11 23:11:31 +0100

    fix hashtag search link

--- twitterLink.php
+++ twitterLink.php
@@ -19,7 +19,7 @@ function linkify_twitter_status($status_text)
   // linkify tags
   $status_text = preg_replace(
     '/(^|\s)#(\w+)/',
-    '\1<a class="twitterpost" href="https://search.twitter.com/search?q=%23\2">#\2</a>',
+    '\1<a class="twitterpost" href="https://twitter.com/search?q=%23\2">#\2</a>',
     $status_text
   );
 
