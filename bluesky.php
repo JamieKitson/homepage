23df5a0cbae69258cc656d94eac99cc8f1b44a65
Jamie Kitson <jamie@kitten-x.com>
2024-10-14 17:33:16 +0100

    bluesky!

---
+++ bluesky.php
@@ -0,0 +1,95 @@
+<?php
+
+include 'blueskypassword.php';
+include 'twitterLink.php';
+
+/*
+$resolveHandle = file_get_contents("https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle=jamiek.it");
+
+echo $resolveHandle;
+
+$id = json_decode($resolveHandle, true)["did"];
+
+echo $id;
+*/
+$id = "did:plc:q5yll4tu6fdivtkx7tpitjhg";
+
+$sessionData = Array("identifier" => $id, "password" => $blueskypassword);
+
+$options = array(
+  'http' => array(
+    'method'  => 'POST',
+    'content' => json_encode( $sessionData ),
+    'header'=>  "Content-Type: application/json"
+    )
+);
+
+$context  = stream_context_create( $options );
+$result = file_get_contents( "https://bsky.social/xrpc/com.atproto.server.createSession", false, $context );
+$response = json_decode( $result, true);
+
+//echo $response["accessJwt"];
+
+$options = array('http' => array(
+    'method'  => 'GET',
+    'header' => 'Authorization: Bearer '.$response["accessJwt"]
+));
+$context  = stream_context_create($options);
+$response = file_get_contents("https://bsky.social/xrpc/app.bsky.feed.getAuthorFeed?actor=jamiek.it&limit=10", false, $context);
+
+//echo $response;
+
+//$response = json_decode( $result);
+
+// file_put_contents( "bluesky.out", $response );
+
+
+    $tweets = json_decode($response);
+
+        foreach($tweets->feed as $tweet)
+        {
+            if (is_array($tweet))
+                throw new Exception($response);
+
+        $date = strtotime($tweet->post->record->createdAt);
+        $status = "";
+        if ($tweet->post->author->handle != "jamiek.it")
+        {
+//            $tweet = $tweet->retweeted_status;
+            $status = "RT @".$tweet->post->author->handle.": ";
+        }
+                echo '<div class="twitterpost">';
+        $status .= $tweet->post->record->text;
+        /*
+        expandURLs($tweet->entities->urls, $status);
+        if (property_exists($tweet->entities, "media"))
+            expandURLs($tweet->entities->media, $status);
+            */
+        echo linkify_twitter_status($status);
+        echo '<div class="twitterdate">';
+        echo statusLink($tweet->post->uri, $tweet->post->author->handle, date('D M d H:i', $date));
+        if (property_Exists($tweet->post, "reply"))
+        {
+                echo statusLink($tweet->in_reply_to_status_id_str, $tweet->in_reply_to_screen_name, 'In reply to '.$tweet->in_reply_to_screen_name);
+        }
+        echo "</div></div>\n";
+    }
+
+function statusLink($statusID, $userName, $linkText)
+{
+        $parts = explode('/', $statusID);
+        $id = end($parts);
+        return sprintf('<a class="twitterdate" href="https://bsky.app/profile/%s/post/%s">%s</a> ',
+            $userName, $id, $linkText);
+}
+
+function expandURLs($urls, &$stat)
+{
+    foreach($urls as $url)
+    {
+        $stat = str_replace($url->url, $url->expanded_url, $stat);
+    }
+}
+
+
+?>
